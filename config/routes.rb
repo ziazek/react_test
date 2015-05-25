b06bf3be156195005953c041059e79573e66e419
Zia Zek <ziazek@gmail.com>
2015-05-25 15:23:09 +0800

    (auto message)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :records
   root to: 'visitors#index'
   get 'about' => 'visitors#about'
   devise_for :users
---
+++ spec/factories/records.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :record do
+    title "MyString"
+date "2015-05-25"
+amount 1.5
+  end
+
+end
